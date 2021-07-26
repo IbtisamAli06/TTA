15a9439c1a65bfb29ba7056f11b8679f6a77dd78
IbtisamAli06 <83982434+IbtisamAli06@users.noreply.github.com>
2021-07-26 12:52:26 +0100

    Add files via upload

--- My first project.sql
+++ My first project.sql
@@ -1,3 +1,8 @@
-SHOW DATABASES;
-use population; 
-explain first_project;
+
+select sec_project.generosity,first_project.generosity
+from first_project
+inner join sec_project
+on first_project.generosity = sec_project.generosity;
+
+
+
