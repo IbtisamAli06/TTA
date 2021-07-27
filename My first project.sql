43ff09c878d9f1f152106145ca2f627ef6655cf3
IbtisamAli06 <83982434+IbtisamAli06@users.noreply.github.com>
2021-07-27 18:48:51 +0100

    Add files via upload

--- My first project.sql
+++ My first project.sql
@@ -1,8 +1,11 @@
-
+SELECT*FROM sec_project,first_project
+where sec_project.generosity = first_project.generosity And sec_project.ID = 10;
 select sec_project.generosity,first_project.generosity
 from first_project
 inner join sec_project
 on first_project.generosity = sec_project.generosity;
+select* from sec_project
+Where feedback between 1 and 9;
 
 
 
